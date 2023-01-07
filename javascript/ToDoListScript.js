ddeafffeb50c14a67c2999de05f11632de8de006
Rocket Primm <primmrock@gmail.com>
2023-01-07 01:21:34 -0500

    Edit and Delete and Style
    Edit and Delete now work. Made style way way better.

--- javascript/ToDoListScript.js
+++ javascript/ToDoListScript.js
@@ -1,61 +1,58 @@
 
 //todo item count
-var todoCount = 1
+var todoCount = 1;
+var edit = false;
+var editTitle;
 
-//adds todo item with information entered in the modal
-$(function() {
-    $('#submit-information').click(function() {
-        //gets title and description from inputs
-        var title = $('#title').val();
-        var description = $('#description').val();
+$(document).ready(function() {
+  //adds todo item
+  $('#add-button').click(function() {
+    $('#add-modal').css('display', 'block');  
+  });
 
-        //adds todo to list with entered data
-        var newDiv = $('<div class="todo-item ' + todoCount + '"><div class="todo-item-left"><input type="checkbox" id="check1"> <label for="check1" id="title'+todoCount+'">Title</label></div><div class="todo-item-right"><i class="fa-solid fa-pen-to-square"></i><i class="fa-solid fa-trash" id="trash"></i></div></div>');
-        $('.todo-container').prepend(newDiv);   
-        $('#title'+todoCount).text(title);
-        todoCount += 1;
-    });
+  //adds todo item with information entered in the modal
+  $('#submit-information').click(function() {
+    // when first creating a todo item
+    if(!edit) {
+      //gets title and description from inputs
+      var title = $('#title').val();
+      var description = $('#description').val();
+
+      //adds todo to list with entered data
+      var newDiv = $('<div class="todo-item ' + todoCount + '"><div class="todo-item-left"><input type="checkbox" id="check1"><label for="check1" id="title'+todoCount+'">Title</label></div><div class="todo-item-right"><div class="edit"><i class="fa-solid fa-pen-to-square"></i></div><div class="trash"><i class="fa-solid fa-trash"></i></div></div></div>');
+      $('.todo-container').prepend(newDiv);   
+      $('#title'+todoCount).text(title);
+      todoCount += 1;
+    //when editing a todo item
+    } else {
+      var title = $('#title').val();
+      var description = $('#description').val();
+      $('#'+editTitle).text(title);
+      edit = false;
+    }
+  });
+
+  //closes modal
+  $('.close').click(function() {
+    $('#add-modal').css('display', 'none');
+    $('#title').val('');
+    $('#description').val('');
+  });
+
+  //deletes item when trashcan is clicked
+  $(document).on('click', '.trash', function() {
+    $(this).parent().parent().remove();
+  });
+  // $('.trash').click(function() {
+  //   $(this).parent().parent().remove();
+  // });
+
+  //allows editing of todo items
+  $(document).on('click', '.edit', function() {
+    edit = true;
+    $('#add-modal').css('display', 'block');
+    editTitle = $(this).parent().parent().find('label').attr('id');
+    $('#title').val($('#'+editTitle).text());
+    // $('#description').val($('#'+editTitle).text());
+  });
 });
-
-//closes modal
-$(function() {
-    $('.close').click(function() {
-         $('#add-modal').css('display', 'none');
-         $('#title').val('');
-         $('#description').val('');
-    });
-});
-
-$(function() {
-    $('#trash').click(function() {
-         var parent = $(this).parent();
-        console.log("cunt");
-    });
-});
-
-
-
-
-/////////////////////////////MODAL CODE////////////////////////////////////////
-
-// Get the modal
-var modal = document.getElementById("add-modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("add-button");
-
-// Get the <span> element that closes the modal
-var close = document.getElementsByClassName("close")[0];
-var close2 = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
